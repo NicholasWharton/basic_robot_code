006b9ee2a581a4fa6d34082ec92048e2b2aa8eb8
Nicholas Wharton <nickggrae@gmail.com>
2016-10-22 21:05:33 -0500

    Fixed spacing so liav isnt butt hurt

--- main (1).cpp
+++ main (1).cpp
@@ -10,7 +10,7 @@ private:
     Joystick* joy1;
     RobotDrive* drive;
     DigitalInput* limitSwitch;
-    AnalogGyro* gyro;
+    AnalogGyro*! gyro;
     Ultrasonic* sonic;
     Compressor* comp;
 
@@ -20,14 +20,14 @@ private:
         backRight = new CANTalon(2);
         backLeft = new CANTalon(3);
         lift = new CANTalon(4)
-        joy1 = new Joystick(1);
+        joy1 = new Joystick(0);
         drive = new RobotDrive(frontRight,frontLeft,backRight,backLeft);
         bool limitSwitch = new DigitalInput(0);
         gyro = new AnalogGyro(3);
         sonic= new Ultrasonic(1,2);
         comp = new Compressor(0)
-
-    }void teleopPeridioc(){
+    }
+    void teleopPeridioc(){
         double leftSide = joy1->GetRawAxis(1);
         double rightSide = joy1->GetRawAxis(5);
         float angle = gyro->GetAngle();
@@ -37,21 +37,24 @@ private:
 
         drive->TankDrive(leftSide,rightSide);
 
-        if(limitswitch = 1){
-        lift->Set(0.5);
+        if(limitswitch == 1){
+            lift->Set(0.5);
 
-        }if(angle = 10){
-        leftSide->Set(-0.5)
-        rightSide->Set(-0.5)
+        }
+        if(angle == 10){
+            leftSide->Set(-0.5)
+            rightSide->Set(-0.5)
 
-        }if(range = 20){
-        lift->Set(-0.5)
+        }
+        if(range == 20){
+            lift->Set(-0.5)
 
-        }if(currentpsi < 100){
-        compre->SetClosedLoopControl(true);
+        }
+        if(currentpsi < 100){
+            compre->SetClosedLoopControl(true);
 
         }else{
-        compre->SetClosedLoopControl(false);
+            compre->SetClosedLoopControl(false);
         }
     }
 };
